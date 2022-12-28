071191f8dc527e7b1187b1ac28580d448b4cc195
jakummer <jakummer@gmail.com>
2022-12-28 17:10:49 -0300

    Entrega LogGzipPerf - Kummer

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ import compression from "compression";
 const app = express();
 //app.use(compression());
 
-app.use(express.json());
+/* app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 const MongoUsers = new DAOUsuarios();
-const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true }; */
+
+
 
 //middleware para logear cualquier llamada
 app.use((req, res, next) =>{
@@ -24,91 +26,6 @@ app.use((req, res, next) =>{
 }
 
 );
-
-
-// app.use(
-//   session({
-//     secret: "2332ee32e3232ee32",
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({
-//       mongoUrl: "mongodb://127.0.0.1:27017/sesiones",
-//       mongoOptions,
-//     }),
-//   })
-// );
-// app.use(express.static("public"));
-
-
-// const { pathname: root } = new URL(".", import.meta.url);
-
-// const authMod = (req, res, next) => {
-//   req.session.rank >= 1
-//     ? next()
-//     : res.status(401).send({ rango: req.session.rank, error: "Sin permisos" });
-// };
-// const authAdmin = (req, res, next) => {
-//   req.session.rank >= 2
-//     ? next()
-//     : res.status(401).send({ rango: req.session.rank, error: "Sin permisos" });
-// };
-
-// app.get("/admin", authAdmin, (req, res) => {
-//   res.send("Hola admin!");
-// });
-// app.get("/mod", authMod, (req, res) => {
-//   res.send("Hola mod!");
-// });
-
-// // root
-// app.get("/", (req, res) => {
- 
-//   if (req.session.usuario) {
-//     res.sendFile(root + "public/dashboard.html");
-//   } else {
-//     if (req.query.error) {
-//       res.sendFile(root + "public/error.html");
-//     } else {
-//       res.sendFile("./public/login.html");
-//     }
-//   }
-// });
-
-
-
-// // * login de usuario
-// app.post("/", async (req, res) => {
-//   // * usuario
-//   // * rango [admin, mod]
-//   try {
-//     const { username, password } = req.body;
-//     const usuario = await MongoUsers.listar(username, password);
-//     console.log(usuario);
-//     req.session.rank = usuario.rank;
-//     req.session.usuario = username;
-//     res.redirect("/");
-//   } catch (e) {
-//     res.redirect("/?error=true");
-//   }
-// });
-
-
-// // * registro de usuario
-// app.post("/register", async (req, res) => {
-//   const { username, password } = req.body;
-//   await MongoUsers.guardar({ username, password });
-//   req.session.usuario = username;
-//   req.session.rank = 0;
-//   res.redirect("/");
-// });
-
-
-// app.get("/logout", (req, res) => {
-//   req.session.destroy((err) => {
-//     res.redirect("/");
-//   });
-// });
-
 
 
 //endpoint para calculo de nro primos
